584ec600292f782dbd05b5724edb97c73560ff7a
Sagar Koli <sagarkoli197@gmail.com>
2024-03-29 15:53:52 +0530

    codeupdatedgi

--- firstProject/firstProject/urls.py
+++ firstProject/firstProject/urls.py
@@ -18,12 +18,13 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
-
+from .views import MyViews
 
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', MyViews.homePage,name="firstHomePage"),
     path('app1/', include("app1.urls")),
 
 ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
